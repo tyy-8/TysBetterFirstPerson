d6d41df1b47ac53b9b57d48332b8bd58ed0fa098
Ty <122072344+tyy-8@users.noreply.github.com>
2025-01-18 00:39:06 +1100

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using BepInEx;
+using Cinemachine;
+using UnityEngine;
+using Utilla;
+
+namespace TysBetterFirstPerson
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private GameObject ShoulderCamera;
+        private GameObject plr;
+        
+        // ToDo: finish the enabled thingy
+        private bool enabled;
+        public void Update()
+        {
+            if (plr == null)
+            {
+                plr = Camera.main.gameObject;
+            }
+            
+            if (ShoulderCamera == null)
+            {
+                ShoulderCamera = GameObject.Find("Shoulder Camera");
+            }
+            else
+            {
+                
+                if (ShoulderCamera.GetComponent<Camera>() != null)
+                {
+                    ShoulderCamera.GetComponent<Camera>().fieldOfView = 110;
+                }
+                
+                ShoulderCamera.GetComponent<CinemachineBrain>().enabled = false;
+                if (plr != null)
+                {
+                    ShoulderCamera.transform.position = Vector3.Lerp(
+                        ShoulderCamera.transform.position,
+                        plr.transform.position + new Vector3(0, 0.1f, 0f) + plr.transform.forward * 0.2f,
+                        Time.deltaTime / 0.005f
+                    );
+                    ShoulderCamera.transform.rotation = Quaternion.Lerp(
+                        ShoulderCamera.transform.rotation,
+                        plr.transform.rotation,
+                        Time.deltaTime / 0.1f
+                    );
+
+                }
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace TysBetterFirstPerson
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.ty.gorillatag.tysbetterfirstperson";
+        public const string Name = "TysBetterFirstPerson";
+        public const string Version = "1.0.0";
+    }
+}
